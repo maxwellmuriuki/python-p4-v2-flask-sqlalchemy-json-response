b396b35357e5de52afca714ede217af61dcec2e0
Your Name <usamax089@gmail.com>
2025-01-20 08:47:35 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -21,12 +22,19 @@ def index():
         200
     )
 
-
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species
+                }
 
+    return make_response(pet_dict, 200)
+
+@app.route('/maxwell_name')
+def is_name():
+    return '<h2>My name is maxwell muriuki and this is my program<h2>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
